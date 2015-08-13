38bfa13c3d0844673d9abda835fcd52fb6ab5be2
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-08-13 15:24:20 -0500

    Added database migrations, that will be performed once the gem is installed

---
+++ db/migrate_legacy/20130711124555_create_ror_maily_tables.rb
@@ -0,0 +1,67 @@
+class CreateRoRmailyTables < ActiveRecord::Migration
+  def change
+    create_table :ror_maily_dispatches do |t|
+      t.string            :type,                                        null: false
+      t.integer           :sequence_id
+      t.string            :context_name
+      t.text              :conditions
+      t.string            :trigger
+      t.string            :mailer_name
+      t.string            :name,                                        null: false
+      t.string            :title
+      t.string            :subject
+      t.string            :from
+      t.text              :template
+      t.integer           :absolute_delay
+      t.datetime          :start
+      t.text              :start_var
+      t.integer           :period
+      t.boolean           :enabled,           default: false
+      t.boolean           :autosubscribe
+      t.boolean           :override_subscription
+      t.integer           :subscription_group_id
+      t.string            :token_action
+
+      t.timestamps
+    end
+		add_index :ror_maily_dispatches, :name, unique: true
+    add_index :ror_maily_dispatches, :context_name
+    add_index :ror_maily_dispatches, :trigger
+
+    create_table :ror_maily_subscriptions do |t|
+      t.string            :type,                                        null: false
+      t.integer           :entity_id,                                   null: false
+      t.string            :entity_type,                                 null: false
+      t.integer           :dispatch_id
+      t.string            :token,                                       null: false
+      t.text              :settings
+      t.text              :data
+      t.boolean           :active,             default: false,       null: false
+      t.datetime          :delivered_at
+
+      t.timestamps
+    end
+		#add_index :ror_maily_subscriptions, [:type, :entity_id, :entity_type, :dispatch_id], unique: true, name: "index_maliy_herald_subscriptions_unique"
+
+    create_table :ror_maily_logs do |t|
+      t.integer           :entity_id,                                   null: false
+      t.string            :entity_type,                                 null: false
+      t.integer           :mailing_id
+      t.string            :status,             default: "delivered", null: false
+      t.text              :data
+      t.datetime          :processed_at
+    end
+
+    create_table :ror_maily_subscription_groups do |t|
+      t.string            :name,                                        null: false
+      t.string            :title,                                       null: false
+    end
+
+    create_table :ror_maily_aggregated_subscriptions do |t|
+      t.integer           :entity_id,                                   null: false
+      t.string            :entity_type,                                 null: false
+      t.integer           :group_id,                                    null: false
+      t.boolean           :active,             default: false,       null: false
+    end
+  end
+end
