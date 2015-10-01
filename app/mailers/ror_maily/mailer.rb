Sology/maily_herald

Refactor internal code handling schedules

app/mailers/maily_herald/mailer.rb
@@ -2,16 +2,16 @@ module MailyHerald
   class Mailer < ActionMailer::Base
     attr_reader :entity
 
-    def generic entity, mailing
-      destination = mailing.destination(entity)
-      subject = mailing.render_subject(entity)
-      content = mailing.render_template(entity)
+    def generic entity
+      destination = @maily_herald_mailing.destination(entity)
+      subject = @maily_herald_mailing.render_subject(entity)
+      content = @maily_herald_mailing.render_template(entity)
 
       opts = {
         to: destination, 
         subject: subject
       }
-      opts[:from] = mailing.from if mailing.from.present?
+      opts[:from] = @maily_herald_mailing.from if @maily_herald_mailing.from.present?
 
       mail(opts) do |format|
         format.text { render text: content }
@@ -23,9 +23,10 @@ module MailyHerald
       def deliver_mail(mail) #:nodoc:
         mailing = mail.maily_herald_data[:mailing]
         entity = mail.maily_herald_data[:entity]
+        schedule = mail.maily_herald_data[:schedule]
 
-        if mailing && entity
-          mailing.deliver_with_mailer_to(entity) do
+        if mailing
+          mailing.send(:deliver_with_mailer, schedule) do
             ActiveSupport::Notifications.instrument("deliver.action_mailer") do |payload|
               self.set_payload_for_mail(payload, mail)
               yield # Let Mail do the delivery actions
@@ -69,20 +70,32 @@ module MailyHerald
         end
       end
 
-      mailing = args[0].to_s == "generic" ? args[2] : MailyHerald.dispatch(args[0])
-      entity = args[1]
+      if args[1].is_a?(MailyHerald::Log)
+        @maily_herald_schedule = args[1]
+        @maily_herald_mailing = @maily_herald_schedule.mailing
+        @maily_herald_entity = @maily_herald_schedule.entity
+      else
+        @maily_herald_mailing = MailyHerald.dispatch(args[0])
+        @maily_herald_entity = args[1]
+
+        if @maily_herald_mailing.respond_to?(:schedule_delivery_to)
+          # Implicitly create schedule for ad hoc delivery when called using Mailer.foo(entity).deliver syntax
+          @maily_herald_schedule = @maily_herald_mailing.schedule_delivery_to(@maily_herald_entity)
+        end
+      end
 
       @_message.maily_herald_data = {
-        mailing: mailing,
-        entity: entity,
-        subscription: mailing.subscription_for(entity),
+        schedule: @maily_herald_schedule,
+        mailing: @maily_herald_mailing,
+        entity: @maily_herald_entity,
+        subscription: @maily_herald_mailing.subscription_for(@maily_herald_entity),
       }
 
       lookup_context.skip_default_locale!
-      super
+      super(args[0], @maily_herald_entity)
 
-      @_message.to = mailing.destination(entity) unless @_message.to
-      @_message.from = mailing.from unless @_message.from
+      @_message.to = @maily_herald_mailing.destination(@maily_herald_entity) unless @_message.to
+      @_message.from = @maily_herald_mailing.from unless @_message.from
 
       @_message
     end

app/models/maily_herald/mailing.rb
@@ -157,11 +157,11 @@ module MailyHerald
     #
     # Depending on {#mailer_name} value it uses either generic mailer (from {Mailer} class)
     # or custom mailer.
-    def build_mail entity
+    def build_mail schedule
       if generic_mailer?
-        Mailer.generic(entity, self)
+        Mailer.generic(schedule)
       else
-        self.mailer.send(self.name, entity)
+        self.mailer.send(self.name, schedule)
       end
     end
 
@@ -172,12 +172,18 @@ module MailyHerald
     # Performs actual sending of emails; should be called in background.
     #
     # Returns `Mail::Message`.
-    def deliver_to entity
-      build_mail(entity).deliver
+    def deliver schedule
+      build_mail(schedule).deliver
+    rescue StandardError => e
+      MailyHerald.logger.log_processing(self, schedule.entity, prefix: "Error", level: :error) 
+      schedule.update_attributes(status: :error, data: {msg: "#{e.to_s}\n\n#{e.backtrace.join("\n")}"})
+      return nil
     end
 
     # Called from Mailer, block required
-    def deliver_with_mailer_to entity
+    def deliver_with_mailer schedule
+      entity = schedule.entity
+
       unless processable?(entity)
         MailyHerald.logger.log_processing(self, entity, prefix: "Not processable", level: :debug) 
         return 
@@ -194,7 +200,7 @@ module MailyHerald
 
       return {status: :delivered, data: {content: mail.to_s}}
     rescue StandardError => e
-      MailyHerald.logger.log_processing(self, entity, prefix: "Error", level: :error) 
+      MailyHerald.logger.log_processing(self, schedule.entity, prefix: "Error", level: :error) 
       return {status: :error, data: {msg: "#{e.to_s}\n\n#{e.backtrace.join("\n")}"}}
     end
 

ssoulless/RoRmaily

app/mailers/ror_maily/mailer.rb
@@ -2,16 +2,16 @@ module RoRmaily
   class Mailer < ActionMailer::Base
     attr_reader :entity
 
-    def generic entity, mailing
-      destination = mailing.destination(entity)
-      subject = mailing.render_subject(entity)
-      content = mailing.render_template(entity)
+    def generic entity
+      destination = @ror_maily_mailing.destination(entity)
+      subject = @ror_maily_mailing.render_subject(entity)
+      content = @ror_maily_mailing.render_template(entity)
 
       opts = {
         to: destination, 
         subject: subject
       }
-      opts[:from] = mailing.from if mailing.from.present?
+      opts[:from] = @ror_maily_mailing.from if @ror_maily_mailing.from.present?
 
       mail(opts) do |format|
         format.text { render text: content }
@@ -23,9 +23,10 @@ module RoRmaily
       def deliver_mail(mail) #:nodoc:
         mailing = mail.ror_maily_data[:mailing]
         entity = mail.ror_maily_data[:entity]
+        schedule = mail.ror_maily_data[:schedule]
 
-        if mailing && entity
-          mailing.deliver_with_mailer_to(entity) do
+        if mailing
+          mailing.send(:deliver_with_mailer, schedule) do
             ActiveSupport::Notifications.instrument("deliver.action_mailer") do |payload|
               self.set_payload_for_mail(payload, mail)
               yield # Let Mail do the delivery actions
@@ -69,20 +70,32 @@ module RoRmaily
         end
       end
 
-      mailing = args[0].to_s == "generic" ? args[2] : RoRmaily.dispatch(args[0])
-      entity = args[1]
+      if args[1].is_a?(RoRmaily::Log)
+        @ror_maily_schedule = args[1]
+        @ror_maily_mailing = @ror_maily_schedule.mailing
+        @ror_maily_entity = @ror_maily_schedule.entity
+      else
+        @ror_maily_mailing = RoRmaily.dispatch(args[0])
+        @ror_maily_entity = args[1]
+
+        if @ror_maily_mailing.respond_to?(:schedule_delivery_to)
+          # Implicitly create schedule for ad hoc delivery when called using Mailer.foo(entity).deliver syntax
+          @ror_maily_schedule = @ror_maily_mailing.schedule_delivery_to(@ror_maily_entity)
+        end
+      end
 
       @_message.ror_maily_data = {
-        mailing: mailing,
-        entity: entity,
-        subscription: mailing.subscription_for(entity),
+        schedule: @ror_maily_schedule,
+        mailing: @ror_maily_mailing,
+        entity: @ror_maily_entity,
+        subscription: @ror_maily_mailing.subscription_for(@ror_maily_entity),
       }
 
       lookup_context.skip_default_locale!
-      super
+      super(args[0], @ror_maily_entity)
 
-      @_message.to = mailing.destination(entity) unless @_message.to
-      @_message.from = mailing.from unless @_message.from
+      @_message.to = @ror_maily_mailing.destination(@ror_maily_entity) unless @_message.to
+      @_message.from = @ror_maily_mailing.from unless @_message.from
 
       @_message
     end

app/models/ror_maily/mailing.rb
@@ -157,11 +157,11 @@ module RoRmaily
     #
     # Depending on {#mailer_name} value it uses either generic mailer (from {Mailer} class)
     # or custom mailer.
-    def build_mail entity
+    def build_mail schedule
       if generic_mailer?
-        Mailer.generic(entity, self)
+        Mailer.generic(schedule)
       else
-        self.mailer.send(self.name, entity)
+        self.mailer.send(self.name, schedule)
       end
     end
 
@@ -172,12 +172,18 @@ module RoRmaily
     # Performs actual sending of emails; should be called in background.
     #
     # Returns `Mail::Message`.
-    def deliver_to entity
-      build_mail(entity).deliver
+    def deliver schedule
+      build_mail(schedule).deliver
+    rescue StandardError => e
+      RoRmaily.logger.log_processing(self, schedule.entity, prefix: "Error", level: :error) 
+      schedule.update_attributes(status: :error, data: {msg: "#{e.to_s}\n\n#{e.backtrace.join("\n")}"})
+      return nil
     end
 
     # Called from Mailer, block required
-    def deliver_with_mailer_to entity
+    def deliver_with_mailer schedule
+      entity = schedule.entity
+
       unless processable?(entity)
         RoRmaily.logger.log_processing(self, entity, prefix: "Not processable", level: :debug) 
         return 
@@ -194,7 +200,7 @@ module RoRmaily
 
       return {status: :delivered, data: {content: mail.to_s}}
     rescue StandardError => e
-      RoRmaily.logger.log_processing(self, entity, prefix: "Error", level: :error) 
+      RoRmaily.logger.log_processing(self, schedule.entity, prefix: "Error", level: :error) 
       return {status: :error, data: {msg: "#{e.to_s}\n\n#{e.backtrace.join("\n")}"}}
     end
 