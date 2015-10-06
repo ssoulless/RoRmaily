Sology/maily_herald

Handle invalid Mailing names.

app/mailers/maily_herald/mailer.rb
@@ -21,6 +21,11 @@ module MailyHerald
     class << self
       #TODO make it instance method so we get access to instance attributes
       def deliver_mail(mail) #:nodoc:
+        unless mail.maily_herald_data
+          MailyHerald.logger.error("Unable to send message. Invalid mailing provided.")
+          return
+        end
+
         mailing = mail.maily_herald_data[:mailing]
         entity = mail.maily_herald_data[:entity]
         schedule = mail.maily_herald_data[:schedule]
@@ -40,11 +45,6 @@ module MailyHerald
           end
         else
           MailyHerald.logger.log_processing(mailing, entity, mail, prefix: "Attempt to deliver email without schedule. No mail was sent", level: :debug)
-
-          #ActiveSupport::Notifications.instrument("deliver.action_mailer") do |payload|
-            #self.set_payload_for_mail(payload, mail)
-            #yield # Let Mail do the delivery actions
-          #end
         end
       end
     end
@@ -53,9 +53,11 @@ module MailyHerald
       return @_message if @_mail_was_called && headers.blank? && !block
 
       # Assign instance variables availabe for template
-      @maily_subscription = @_message.maily_herald_data[:subscription]
-      @maily_entity = @_message.maily_herald_data[:entity]
-      @maily_mailing = @_message.maily_herald_data[:mailing]
+      if @_message.maily_herald_data
+        @maily_subscription = @_message.maily_herald_data[:subscription]
+        @maily_entity = @_message.maily_herald_data[:entity]
+        @maily_mailing = @_message.maily_herald_data[:mailing]
+      end
 
       super
     end
@@ -84,18 +86,22 @@ module MailyHerald
         @maily_herald_entity = args[1]
       end
 
-      @_message.maily_herald_data = {
-        schedule: @maily_herald_schedule,
-        mailing: @maily_herald_mailing,
-        entity: @maily_herald_entity,
-        subscription: @maily_herald_mailing.subscription_for(@maily_herald_entity),
-      }
+      if @maily_herald_mailing
+        @_message.maily_herald_data = {
+          schedule: @maily_herald_schedule,
+          mailing: @maily_herald_mailing,
+          entity: @maily_herald_entity,
+          subscription: @maily_herald_mailing.subscription_for(@maily_herald_entity),
+        }
+      end
 
       lookup_context.skip_default_locale!
       super(args[0], @maily_herald_entity)
 
-      @_message.to = @maily_herald_mailing.destination(@maily_herald_entity) unless @_message.to
-      @_message.from = @maily_herald_mailing.from unless @_message.from
+      if @maily_herald_mailing
+        @_message.to = @maily_herald_mailing.destination(@maily_herald_entity) unless @_message.to
+        @_message.from = @maily_herald_mailing.from unless @_message.from
+      end
 
       @_message
     end

spec/mailers/maily_herald/mailer_spec.rb
@@ -30,4 +30,16 @@ describe MailyHerald::Mailer do
       expect(MailyHerald::Log.delivered.count).to eq(1)
     end
   end
+
+  context "without defined mailing" do
+    it "should not deliver" do
+      expect do
+        expect(MailyHerald::Log.delivered.count).to eq(0)
+
+        AdHocMailer.missing_mailing_mail(@entity).deliver
+
+        expect(MailyHerald::Log.delivered.count).to eq(0)
+      end.not_to change { ActionMailer::Base.deliveries.count }
+    end
+  end
 end

ssoulless/RoRmaily

app/mailers/maily_herald/mailer.rb
@@ -21,6 +21,11 @@ module RoRmaily
     class << self
       #TODO make it instance method so we get access to instance attributes
       def deliver_mail(mail) #:nodoc:
+        unless mail.ror_maily_data
+          RoRmaily.logger.error("Unable to send message. Invalid mailing provided.")
+          return
+        end
+
         mailing = mail.ror_maily_data[:mailing]
         entity = mail.ror_maily_data[:entity]
         schedule = mail.ror_maily_data[:schedule]
@@ -40,11 +45,6 @@ module RoRmaily
           end
         else
           RoRmaily.logger.log_processing(mailing, entity, mail, prefix: "Attempt to deliver email without schedule. No mail was sent", level: :debug)
-
-          #ActiveSupport::Notifications.instrument("deliver.action_mailer") do |payload|
-            #self.set_payload_for_mail(payload, mail)
-            #yield # Let Mail do the delivery actions
-          #end
         end
       end
     end
@@ -53,9 +53,11 @@ module RoRmaily
       return @_message if @_mail_was_called && headers.blank? && !block
 
       # Assign instance variables availabe for template
-      @maily_subscription = @_message.ror_maily_data[:subscription]
-      @maily_entity = @_message.ror_maily_data[:entity]
-      @maily_mailing = @_message.ror_maily_data[:mailing]
+      if @_message.ror_maily_data
+        @maily_subscription = @_message.ror_maily_data[:subscription]
+        @maily_entity = @_message.ror_maily_data[:entity]
+        @maily_mailing = @_message.ror_maily_data[:mailing]
+      end
 
       super
     end
@@ -84,18 +86,22 @@ module RoRmaily
         @ror_maily_entity = args[1]
       end
 
-      @_message.ror_maily_data = {
-        schedule: @ror_maily_schedule,
-        mailing: @ror_maily_mailing,
-        entity: @ror_maily_entity,
-        subscription: @ror_maily_mailing.subscription_for(@ror_maily_entity),
-      }
+      if @ror_maily_mailing
+        @_message.ror_maily_data = {
+          schedule: @ror_maily_schedule,
+          mailing: @ror_maily_mailing,
+          entity: @ror_maily_entity,
+          subscription: @ror_maily_mailing.subscription_for(@ror_maily_entity),
+        }
+      end
 
       lookup_context.skip_default_locale!
       super(args[0], @ror_maily_entity)
 
-      @_message.to = @ror_maily_mailing.destination(@ror_maily_entity) unless @_message.to
-      @_message.from = @ror_maily_mailing.from unless @_message.from
+      if @ror_maily_mailing
+        @_message.to = @ror_maily_mailing.destination(@ror_maily_entity) unless @_message.to
+        @_message.from = @ror_maily_mailing.from unless @_message.from
+      end
 
       @_message
     end

spec/mailers/ror_maily/mailer_spec.rb
@@ -30,4 +30,16 @@ describe RoRmaily::Mailer do
       expect(RoRmaily::Log.delivered.count).to eq(1)
     end
   end
+
+  context "without defined mailing" do
+    it "should not deliver" do
+      expect do
+        expect(RoRmaily::Log.delivered.count).to eq(0)
+
+        AdHocMailer.missing_mailing_mail(@entity).deliver
+
+        expect(RoRmaily::Log.delivered.count).to eq(0)
+      end.not_to change { ActionMailer::Base.deliveries.count }
+    end
+  end
 end